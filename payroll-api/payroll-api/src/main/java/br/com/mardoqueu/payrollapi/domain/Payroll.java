6d88f2b571f0473a28d399951ed98eaaf5dfcd74
mardoqueu <mdq767@gmail.com>
2022-09-30 09:34:52 -0300

    Payroll-API: starting project

---
+++ payroll-api/payroll-api/src/main/java/br/com/mardoqueu/payrollapi/domain/Payroll.java
@@ -0,0 +1,17 @@
+package br.com.mardoqueu.payrollapi.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter @Setter
+@AllArgsConstructor
+public class Payroll {
+
+    private String workerName;
+    private String description;
+    private Double hourlyPrice;
+    private Double workedHours;
+    private Double totalPayment;
+}
---
+++ payroll-api/payroll-api/src/test/java/br/com/mardoqueu/payrollapi/PayrollApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mardoqueu.payrollapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayrollApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
