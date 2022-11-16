a78cddbbe748d4b2e39b8882cdb5e32a562789f3
Mardoqueu Sousa <mdq767@gmail.com>
2022-11-16 11:11:19 -0300

    load balancing

--- user-api/user-api/src/main/java/br/com/mardoqueu/userapi/services/impl/UserServiceImpl.java
+++ user-api/user-api/src/main/java/br/com/mardoqueu/userapi/services/impl/UserServiceImpl.java
@@ -5,19 +5,26 @@ import br.com.mardoqueu.userapi.reprositories.UserRepository;
 import br.com.mardoqueu.userapi.services.UserService;
 import br.com.mardoqueu.userapi.services.exceptions.ObjectNotFoundException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Slf4j
 @RequiredArgsConstructor
 @Service
+
 public class UserServiceImpl implements UserService {
 
     private final UserRepository repository;
+    private final Environment env;
+
 
     @Override
     public User findById(Long id) {
+        log.info("USER_SERVICE ::: Get request on " + env.getProperty("local.server.port") + " port");
         return repository.findById(id).orElseThrow(() -> new ObjectNotFoundException("Object not found"));
     }
 
