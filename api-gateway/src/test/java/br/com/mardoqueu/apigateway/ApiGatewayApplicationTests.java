83ddc5b007de07d30e0bd3dea65718d052e624ef
Mardoqueu Sousa <mdq767@gmail.com>
2022-11-16 18:47:06 -0300

    api gateway

---
+++ api-gateway/src/test/java/br/com/mardoqueu/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mardoqueu.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
